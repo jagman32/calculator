b57af9c4a2b640520d05db657bafa0ec59eef4a5
Gurcharan Singh <you@example.com>
2021-06-10 15:29:49 +0530

    Calculator template Added

---
+++ calculator.py
@@ -0,0 +1,12 @@
+class calculator:
+    def addition(a,b):
+        pass
+    
+    def multiplication(a,b):
+        pass
+
+    def subtraction(a,b):
+        pass
+
+    def division(a,b):
+        pass
