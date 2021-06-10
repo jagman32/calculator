ce6b77ab423180ddebe5944d0cf79d3e16cf1071
Gurcharan Singh <you@example.com>
2021-06-10 16:02:28 +0530

    Declared all functions as staticmethods

--- calculator.py
+++ calculator.py
@@ -1,12 +1,17 @@
 class calculator:
+
+    @staticmethod
     def addition(a,b):
         return a+b
     
+    @staticmethod
     def multiplication(a,b):
         return a*b
 
+    @staticmethod
     def subtraction(a,b):
         pass
 
+    @staticmethod
     def division(a,b):
         pass
